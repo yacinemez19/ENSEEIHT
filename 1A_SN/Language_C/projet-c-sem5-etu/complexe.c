0e0b8ac307b37dfe85e2e9b10bf6a52b520ce340
Yacine Meziani <135065821+yacinemez19@users.noreply.github.com>
2024-12-03 16:52:07 +0100

    up ajouter complexe.c

--- 1A_SN/Language_C/projet-c-sem5-etu/complexe.c
+++ 1A_SN/Language_C/projet-c-sem5-etu/complexe.c
@@ -39,7 +39,7 @@ void conjugue(complexe_t* resultat, complexe_t op) {
 
 
 void ajouter(complexe_t* resultat, complexe_t gauche, complexe_t droite) {
-    init(resultat, reelle(gauche) + reelle(droite), reelle(gauche) + imaginaire(droite));
+    init(resultat, reelle(gauche) + reelle(droite), imaginaire(gauche) + imaginaire(droite));
 }
 
 
